5b91210ef7282ff20bc0805327630285f7af56bc
Matthew Eastwood <ilovelego@outlook.com>
2025-11-05 21:02:05 -0500

    create post and comment classes

---
+++ Blogging-Web-API.Core/Models/Comment.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Blogging_Web_API.Core.Models;
+
+public class Comment
+{
+    public int Id { get; set; }
+
+    [Required]
+    public int PostId { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    [MaxLength(150)]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(1000)]
+    public string Content { get; set; } = string.Empty;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public Post? Post { get; set; }
+}
---
+++ Blogging-Web-API.Core/Models/Post.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.Transactions;
+
+namespace Blogging_Web_API.Core.Models;
+
+public class Post
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public string Title { get; set; } = string.Empty;
+
+    [Required]
+    public string Content { get; set; } = string.Empty;
+
+    [Required]
+    public string Author { get; set; } = "Author";
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public ICollection<Comment>? Comments { get; set;}
+}
