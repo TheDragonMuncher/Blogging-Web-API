7b694b4c2592a5d8bb6ab5dadf4676be336c00e3
Matthew Eastwood <ilovelego@outlook.com>
2025-11-07 21:51:02 -0500

    create migrations and database

--- Blogging-Web-API.Infrastructure/Data/AppDbContext.cs
+++ Blogging-Web-API.Infrastructure/Data/AppDbContext.cs
@@ -8,8 +8,8 @@ public class AppDbContext: DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    public DbSet<Post> Posts;
-    public DbSet<Comment> Comments;
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Comment> Comments { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
